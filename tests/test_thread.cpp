d70fd804763901d3debce4d656ae2beaede84360
Kinvy <kinvy66@163.com>
2023-04-18 18:27:07 +0800

    error: 日志配置加载后，日志失效

--- tests/test_thread.cpp
+++ tests/test_thread.cpp
@@ -9,44 +9,37 @@
 #include "sylar/sylar.h"
 #include <unistd.h>
 
-sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
 
 int count = 0;
 sylar::RWMutex s_mutex;
 
 void fun1() {
-    SYLAR_LOG_INFO(g_logger) << "name: " << sylar::Thread::GetName()
-                            << " this.name: " << sylar::Thread::GetThis()->GetName()
-                            << " id: " << sylar::GetThreadId()
-                            << " this.id: " << sylar::Thread::GetThis()->getId();
-    for (int i = 0; i < 100000; ++i) {
-        sylar::RWMutex::WriteLock lock(s_mutex);
-        ++count;
-    }
+    // SYLAR_LOG_INFO(g_logger) << "name: " << sylar::Thread::GetName()
+    //                         << " this.name: " << sylar::Thread::GetThis()->GetName()
+    //                         << " id: " << sylar::GetThreadId()
+    //                         << " this.id: " << sylar::Thread::GetThis()->getId();
+    // for (int i = 0; i < 100000; ++i) {
+    //     sylar::RWMutex::WriteLock lock(s_mutex);
+    //     ++count;
+    // }
 }
 
 void fun2() {
 
 }
 
-int main(int argc, char** argv) {
-
+void test() {
+    // sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+    // sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("root");
+    sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
     SYLAR_LOG_INFO(g_logger) << "thread test begin";
+    YAML::Node root = YAML::LoadFile("./conf/log2.yml");
+    sylar::Config::LoadFromYaml(root);
+    SYLAR_LOG_INFO(g_logger) << "================================";
+}
 
-    std::vector<sylar::Thread::ptr> thrs;
-
-    for(int i = 0; i < 5; ++i) {
-        sylar::Thread::ptr thr(new sylar::Thread(&fun1, "name_"+std::to_string(i)));
-        thrs.push_back(thr);
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        thrs[i]->join();
-    }
-    SYLAR_LOG_INFO(g_logger) << "thread test end";
-    SYLAR_LOG_INFO(g_logger) << "count=" << count;
-
+int main(int argc, char** argv) {
+   test();
     return 0;
 }
 
-// 回去t_thread_name = thread->getName();加上，还有Thread构造函数里没赋值/
